95fd9fb01841ba3c962366a74ed50f8490275eb7
elias <elias.jm.garcia@gmail.com>
2018-03-27 23:35:14 -0500

    fixed code, added extra optional constructor parameters

--- tomaty/tomaty_notebook.py
+++ tomaty/tomaty_notebook.py
@@ -15,15 +15,15 @@ from gi.repository import Gtk, GObject, GLib, Gdk
 
 
 class TomatyNotebook(Gtk.Notebook):
-    def __init__(self):
+    def __init__(self, width=0, height=0):
         super(TomatyNotebook, self).__init__()
 
-        self.set_size_request(250, 150)
+        self.set_size_request(width, height)
 
 
 class TomatyPage(Gtk.VBox):
-    def __init__(self):
+    def __init__(self, spacing=0, homogeneous=False):
         "docstring"
         super(TomatyPage, self).__init__()
-        self.spacing = 0
-        self.set_homogeneous(False)
+        self.set_spacing(spacing)
+        self.set_homogeneous(homogeneous)
