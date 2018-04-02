3720d8083dc2e1a9dfdb3bbdfedd0450420b119d
elias <elias.jm.garcia@gmail.com>
2018-04-02 10:03:45 -0500

    fixed total_time counter yet again (mixed up time value attributes)

--- tomaty/tomaty.py
+++ tomaty/tomaty.py
@@ -194,7 +194,7 @@ class Tomaty(Gtk.Window):
             if self.breakPeriod:
                 self.timerLabel.set_markup(str=BREAK_MSG)
                 self.breakPeriod = False
-                self.total_time = self.total_time + self.tomaTime
+                self.total_time = self.total_time + self.breakPeriod
                 self.totalLabel.set_markup(
                     str=TOTAL_TIME.format(str(self.total_time)))
             else:
@@ -202,7 +202,7 @@ class Tomaty(Gtk.Window):
                 self.countLabel.set_markup(
                     str=COUNT.format(self.tomatosCompleted))
 
-                self.total_time = self.total_time + self.breakTime
+                self.total_time = self.total_time + self.tomaTime
                 self.totalLabel.set_markup(
                     str=TOTAL_TIME.format(str(self.total_time)))
 
